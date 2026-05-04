95d5f9cf3560dd5f0d2a74c97089be9bf97b3790
Jose Varela <ijosevp@gmail.com>
2026-05-03 20:31:01 -0400

    Initial Setup Commit

---
+++ include/AsyncLogger/AsyncLogger.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include <iostream>
+
+class Logger
+{
+public:
+	void LogInfo(const std::string& message)
+	{
+		std::cout << "[INFO] : " << message << '\n';
+	}
+};
---
+++ src/AsyncLogger.cpp
@@ -0,0 +1,12 @@
+﻿#include "AsyncLogger/AsyncLogger.h"
+
+using namespace std;
+
+int main()
+{
+	Logger logger;
+
+	logger.LogInfo("Hello there buddy!");
+
+	return 0;
+}
