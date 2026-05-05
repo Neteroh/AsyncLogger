503c0db5273e04112bd0ea920db38d2a6431e6d0
Jose Varela <ijosevp@gmail.com>
2026-05-04 20:26:52 -0400

    Refactor logger with LogMessage struct and unified Log() API
    - Added LogMessage for structured logging data
    - Centralized logging through Log() function
    - Included timestamp and thread ID in output
    - Simplified enum-to-string conversion
    - Reduced copies by using const references

--- src/AsyncLogger.cpp
+++ src/AsyncLogger.cpp
@@ -6,7 +6,43 @@ int main()
 {
 	Logger logger;
 
-	logger.LogInfo("Hello there buddy!");
+	logger.Log(DEBUG, "This is me debugging buddy!");
+	logger.Log(INFO, "Hello there buddy, this is just info!");
+	logger.Log(WARNING, "Warning buddy, this is a warning!");
+	logger.Log(ERROR, "Buddy i think you have an error!");
+	logger.Log(CRITICAL, "Buddy you have to leave this is critical!");
 
 	return 0;
+}
+
+LogMessage::LogMessage(std::string message,
+	LogLevel logLevel,
+	std::chrono::time_point<std::chrono::system_clock> timestamp,
+	std::thread::id threadId)
+	: message_(std::move(message)),
+	logLevel_(std::move(logLevel)),
+	timestamp_(std::move(timestamp)),
+	threadId_(std::move(threadId))
+{
+
+}
+
+const std::string& LogMessage::message() const noexcept
+{
+	return message_;
+}
+
+LogLevel LogMessage::logLevel() const noexcept
+{
+	return logLevel_;
+}
+
+std::chrono::time_point<std::chrono::system_clock> LogMessage::timestamp() const noexcept
+{
+	return timestamp_;
+}
+
+std::thread::id LogMessage::threadId() const noexcept
+{
+	return threadId_;
 }
